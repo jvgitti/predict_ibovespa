0da61e9020a78fccf2351af8eeef4573aaa384fa
keldias <56850061+keldias@users.noreply.github.com>
2023-09-02 15:27:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,12 +48,18 @@ with tab_0:
 
     """
     Para esta analise, extraimos 20 anos de dados. Esse conjunto foi suficiente para entender o comportamento da bolsa de valores IBOVESPA e 
-    entender as principais tendencias. Conforme grafico acima, podemos observar que em linhas gerais temos uma tendencia de aumento na bolsa dese 2004.
-    Entretanto a bolsa de valores foi marcada por uma series de fatores, e foi diretamente impactada por eles. 
+    entender as principais tendencias, principalmente porque os periodos de 2013 a 2023 foi marcado por algumas grandes quedas na bola. Conforme grafico 
+    acima, podemos observar que em linhas gerais temos uma tendencia de aumento na bolsa dese 2004.
+    Entretanto a bolsa de valores foi marcada por uma series de fatores, e foi diretamente impactada por eles, abaixo temos uma representaçao de 5 quedas enfrentadas
+    pelo IBOVESPA. De acordo com uma noticia divulgada pela propria B3: "A maior queda, de 22,26%, foi registrada no dia 21 de março de 1990, quando o Plano Collor foi anunciado. 
+    Recentemente, a maior queda foi de 13,92%, em 16 de março de 2020, repercutindo a incerteza diante da pandemia."
 
-    
     """
     st.image("https://www.b3.com.br/data/files/42/20/55/D4/E0AB8810C7AB8988AC094EA8/Linha%20do%20Tempo%20Ibovespa%20B3.png")
+    """
+    fonte: https://www.b3.com.br/pt_br/noticias/ibovespa-b3-completa-55-anos-veja-10-curiosidades-sobre-o-indice-mais-importante-do-mercado-de-acoes-brasileiro.htm
+    """
+    
     """
     Decompondo a série temporal, para uma sazonalidade de 1 ano:
     """
